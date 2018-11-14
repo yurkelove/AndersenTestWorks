ebb4cd50ac5013662c95dee71ebc015a6f9b5012
Yura Kuzkin <yurkelove28@gmail.com>
2018-11-14 11:59:45 +0200

    arrow func

--- numbersSort/js/numbers.js
+++ numbersSort/js/numbers.js
@@ -4,43 +4,33 @@ function numbersSort(){
 
     const diapason = Math.floor(Math.random() * (60 - 40)) + 40;
     const result = Array.apply(null, Array(diapason)).map(function(){ 
-        const numbers = Math.random() * 100 % 100;
-        const fixed = numbers.toFixed(2);
+        const number = Math.random() * 100 % 100;
+        const fixed = number.toFixed(2);
         return Number(fixed);
         // Преобразовать в число
     })
-    console.log(result);
-    
-    const sortNumbers = result.sort(function(a,b){
-        return a-b;
-    });
 
-    // const filtredNumbers = result.filter(number => number >= 30 );
-    const filtredNumbers = sortNumbers.filter(function(number){
-     return number >= 30;
-    });
+    const sortNumbers = result.sort((a,b) => a - b);
 
-    const summ = filtredNumbers.reduce(function(a,b){
-        return a + b;
-    },0);
-    console.log('Сумма :' + ' ' + summ);
+    const filtredNumbers = sortNumbers.filter(number => number >= 30 );
+
+    const summ = filtredNumbers.reduce((a,b) => a + b ,0);
+
 
     const valute = filtredNumbers.map(function(num){
-        return num + ' грн';
+        return `${num} ${`грн`}` ;//Использовать шаблоной строки
     })
-    console.log(valute);
 
-
-    Object.keys(valute).forEach(function eachKey(key) { 
+    valute.forEach(function(price,index) { 
         
-        let table = document.getElementById("myTable");
-        let row = table.insertRow(key);
-        let cell1 = row.insertCell(0);
-        let cell2 = row.insertCell(1);
+        const table = document.getElementById("myTable");
+        const row = table.insertRow(index);
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
 
-        cell1.innerHTML = key;
-        cell2.innerHTML = valute[key];
-        let total = document.getElementById('total__summ');
+        cell1.innerHTML = index;
+        cell2.innerHTML = price;
+        const total = document.getElementById('total__summ');
         total.innerHTML = summ.toFixed(2); 
 
     });
