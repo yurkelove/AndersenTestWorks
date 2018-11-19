5bb070fd2ae04fc00c9b12f46d2d40285981ef0b
Yuriy Kuzkin <yurkelove28@gmail.com>
2018-11-19 11:46:21 +0200

    eslint and timeout

--- Ajax-Promise/js/Ajax-promise.js
+++ Ajax-Promise/js/Ajax-promise.js
@@ -1,61 +1,69 @@
-
-
-let batman = 'http://127.0.0.1:8080/urls/url_1.html';
-let superman = 'http://127.0.0.1:8080/urls/url_2.html';
-let vinniPuh = 'http://127.0.0.1:8080/urls/url_3.html';
-let mikkyMouse = 'http://127.0.0.1:8080/urls/url_4.html';
-let bugsBunny = 'http://127.0.0.1:8080/urls/url_5.html';
+const fetch = require('isomorphic-fetch');
+const batman = 'http://127.0.0.1:8080/urls/url_1.html';
+const superman = 'http://127.0.0.1:8080/urls/url_2.html';
+const vinniPuh = 'http://127.0.0.1:8080/urls/url_3.html';
+const mikkyMouse = 'http://127.0.0.1:8080/urls/url_4.html';
+const bugsBunny = 'http://127.0.0.1:8080/urls/url_5.html';
 
 const siteUrls = [
   batman,
   superman,
   vinniPuh,
   mikkyMouse,
-  bugsBunny
-] 
+  bugsBunny,
+];
 
-const fetchUrls = url => fetch(url,{credentials: 'include'}).then(urls => urls.text());
+const fetchUrls = url => fetch(url, { credentials: 'include' }).then(urls => urls.text());
 const promise_all = siteUrls.map(fetchUrls);
 const promise_race = siteUrls.map(fetchUrls);
 
-const promiseAll = function(){
-        const returnAll = Promise.all(promise_all).then(results => {
-              console.log(results); 
-        },error => {
-                console.log(error)
-        });
-        return returnAll;
+const promiseAll = () => {
+  const returnAll = Promise.all(promise_all).then((results) => {
+    console.log(results) 
+  }, (error) => {
+    console.log(error);
+  });
+  return returnAll;
 }
 
        
-const promiseRace = function(){
-        const returnRace = Promise.race(promise_race).then(results => {
-                console.log( results );
-        },error => {
-                console.log(error);
-        });
-        return returnRace;      
-}
+const promiseRace = () => {
+  const returnRace = Promise.race(promise_race).then((results) => {
+    console.log(results)
+  }, (error) => {
+    console.log(error);
+  });
+  return returnRace;      
+};
 
 
-const promiseChain = function(){
-        let startChain = Promise.resolve();
-        siteUrls.forEach(url => {
-        startChain = startChain
-        .then(() => fetch(url))
-        .then(results => {
-                console.log(results);
-        },error => {
-                console.log(error);
-        })
-        });
-}
+const promiseChain = () => {
+  let startChain = Promise.resolve();
+  siteUrls.forEach((url) => {
+    startChain = startChain
+      .then(() => fetch(url))
+      .then((results) => {
+        console.log(results);
+      }, (error) => {
+        console.log(error);
+      });
+  });
+};
 
 
+const timer = () => {
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+  return promise;
+};
 
-promiseAll() 
-        .then(promiseRace)
-        .then(promiseChain)
-      
-
+timer()
+  .then(promiseAll)
+  .then(timer)
+  .then(promiseRace)
+  .then(timer)
+  .then(promiseChain);
 
