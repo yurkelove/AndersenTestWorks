9e33055be06df11b307e4fe52bd12303e738ec38
Yura Kuzkin <yurkelove28@gmail.com>
2018-11-20 17:47:20 +0200

    eslint change files

--- cookies-local/js/cookieLocal.js
+++ cookies-local/js/cookieLocal.js
@@ -1,62 +1,51 @@
 
-	const date = new Date;
-	date.setDate(date.getDate() + 1);
-	document.cookie = "User = Yura;"
-	document.cookie = "Gender = Male;"
-	document.cookie = "City = Chernihiv;"
-	document.cookie = "Date = " + date;
-	document.cookie = "Company = Andersen;"
-	document.cookie = "Greet = Hello,today is monday"
-	
-	
-	
-	
+const date = new Date;
+date.setDate(date.getDate() + 1);
+document.cookie = 'User = Yura;';
+document.cookie = 'Gender = Male;';
+document.cookie = 'City = Chernihiv;';
+document.cookie = 'Date = ' + date;
+document.cookie = 'Company = Andersen;';
+document.cookie = 'Greet = Hello,today is monday';
+
 
 function cookiesToLocalStorage(){
 
-	const cookieStr = document.cookie;
-
-	
-	const cookieSplit = cookieStr.split('; ');
-	for(let i = 0; i < cookieSplit.length; i++ ){
-		const cookieSingle = cookieSplit[i];
-		let symbol = cookieSingle.indexOf("=");
-		let keyCookie = cookieSingle.substring(0,symbol);
-		let valueCookie = cookieSingle.substring(symbol+1);
-		valueCookie = decodeURIComponent(valueCookie);
-		let lGet = localStorage.getItem(keyCookie);
-		if(lGet === undefined || lGet === null){
-			localStorage.setItem(keyCookie,valueCookie);
-		}
-
-
-	}
-
+  const cookieStr = document.cookie;
+  const cookieSplit = cookieStr.split('; ');
+  for (let i = 0; i < cookieSplit.length; i++) {
+    const cookieSingle = cookieSplit[i];
+    const symbol = cookieSingle.indexOf('=');
+    const keyCookie = cookieSingle.substring(0, symbol);
+    let valueCookie = cookieSingle.substring(symbol + 1);
+    valueCookie = decodeURIComponent(valueCookie);
+    const lGet = localStorage.getItem(keyCookie);
+    if (lGet === undefined || lGet === null) {
+      localStorage.setItem(keyCookie, valueCookie);
+    }
+  }
 }
 
 cookiesToLocalStorage();
 
 
-
 function getCookie(name) {
-	var matches = document.cookie.match(new RegExp(
-	  "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-	));
-	return matches ? decodeURIComponent(matches[1]) : undefined;
+  let matches = document.cookie.match(new RegExp(
+    '(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)'
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
 
-function localStorageToCookies(){
-	const len = localStorage.length;
-	for(let i = 0 ; i < len; i++){
-		const key = localStorage.key(i);
-		const name = localStorage.getItem(key);
-		if (getCookie(key) !== undefined) {
-		document.cookie = `${key} = ${name}`;
-		}
-	}
+function localStorageToCookies() {
+  const len = localStorage.length;
+  for (let i = 0; i < len; i++) {
+    const key = localStorage.key(i);
+    const name = localStorage.getItem(key);
+    if (getCookie(key) !== undefined) {
+      document.cookie = `${key} = ${name}`;
+    }
+  }
 }
 
 localStorageToCookies();
-
-console.log(document.cookie);
--- numbersSort/js/numbers.js
+++ numbersSort/js/numbers.js
@@ -1,39 +1,27 @@
 
 
-function numbersSort(){
-
-    const arrayLength = Math.floor(Math.random() * (60 - 40)) + 40;
-    
-    const result = [...Array(arrayLength)].map(() => {
-        const number = Math.random() * 100;
-        return Math.round(number*100)/100;
-    });
-          
-    const sortNumbers = result.sort((a,b) => a - b);
-
-    const filtredNumbers = sortNumbers.filter(number => number >= 30);
-
-    const summ = filtredNumbers.reduce((a,b) => a + b, 0);
-    
-    const prices = filtredNumbers.map(num => `${num} грн`);
-
-    prices.forEach((price,index) => { 
-        
-        const table = document.getElementById("myTable");
-        const row = table.insertRow(index);
-        const cell1 = row.insertCell(0);
-        const cell2 = row.insertCell(1);
-
-        cell1.innerHTML = index;
-        cell2.innerHTML = price;
-        const total = document.getElementById('total__summ');
-        total.innerHTML = Math.round(summ*100)/100;  
-
-    });
+function numbersSort() {
+  const arrayLength = Math.floor(Math.random() * (60 - 40)) + 40;
+  const result = [...Array(arrayLength)].map(() => {
+    const number = Math.random() * 100;
+    return Math.round(number * 100) / 100;
+  });
+  const sortNumbers = result.sort((a, b) => a - b);
+  const filtredNumbers = sortNumbers.filter(number => number >= 30);
+  const summ = filtredNumbers.reduce((a, b) => a + b, 0);
+  const prices = filtredNumbers.map(num => `${num} грн`);
+  prices.forEach((price, index) => {
+    const table = document.getElementById('myTable');
+    const row = table.insertRow(index);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    cell1.innerHTML = index;
+    cell2.innerHTML = price;
+    const total = document.getElementById('total__summ');
+    total.innerHTML = Math.round(summ*100) / 100;
+  });
 }
 
+
 numbersSort();
-
-
-
 
